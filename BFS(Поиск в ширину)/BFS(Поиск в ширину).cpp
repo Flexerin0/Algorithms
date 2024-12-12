beb8dc4c12b212d979455d1ff160bb72fd13ba92
Flexerin0 <vfieliks@mail.ru>
2024-12-12 23:36:30 +0300

    Added algorithms

---
+++ BFS(Поиск в ширину)/BFS(Поиск в ширину).cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void addEdge(vector<vector<int>>& graph, int u, int v) {
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+}
+
+vector<int> bfs(const vector<vector<int>>& graph, int start) {
+    int n = graph.size();
+    vector<bool> visited(n, false);
+    vector<int> reachable;
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        reachable.push_back(v);
+
+        for (int neighbor : graph[v]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+
+    return reachable;
+}
+
+void printResult(const vector<int>& reachable, int start) {
+    cout << "Вершины, достижимые из вершины " << start << ":\n";
+    for (int vertex : reachable) {
+        cout << vertex << " ";
+    }
+    cout << endl;
+}
+
+void testBFS() {
+    int numVertices = 8;
+    vector<vector<int>> graph(numVertices);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 5);
+    addEdge(graph, 5, 6);
+    addEdge(graph, 6, 7);
+
+    int startVertex = 0;
+    vector<int> reachable = bfs(graph, startVertex);
+
+    printResult(reachable, startVertex);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    testBFS();
+
+    return 0;
+}
---
+++ Алгоритм Дейкстры(Куча)/Алгоритм Дейкстры(Куча).cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+const int INF = numeric_limits<int>::max();
+
+void addEdge(vector<vector<pair<int, int>>>& graph, int u, int v, int weight) {
+    graph[u].emplace_back(v, weight);
+}
+
+vector<int> dijkstra(const vector<vector<pair<int, int>>>& graph, int start) {
+    int n = graph.size();
+    vector<int> len(n, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+    len[start] = 0;
+    pq.emplace(0, start);
+
+    while (!pq.empty()) {
+        pair<int, int> current = pq.top();
+        pq.pop();
+
+        int currentLen = current.first;
+        int v = current.second;
+
+        if (currentLen > len[v]) {
+            continue;
+        }
+
+        for (const auto& edge : graph[v]) {
+            int to = edge.first;
+            int weight = edge.second;
+
+            if (len[v] + weight < len[to]) {
+                len[to] = len[v] + weight;
+                pq.emplace(len[to], to);
+            }
+        }
+    }
+
+    return len;
+}
+
+void printResult(const vector<int>& distances, int start) {
+    cout << "Кратчайшее расстояние от начальной вершины " << start << ":\n";
+    for (int i = 0; i < distances.size(); ++i) {
+        cout << "Вершина " << i << ": ";
+        if (distances[i] == INF) {
+            cout << "INF";
+        }
+        else {
+            cout << distances[i];
+        }
+        cout << endl;
+    }
+}
+
+void testDijkstra() {
+    int numsVertex = 10;
+    vector<vector<pair<int, int>>> graph(numsVertex);
+
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 2, 1);
+    addEdge(graph, 2, 1, 2);
+    addEdge(graph, 1, 3, 1);
+    addEdge(graph, 2, 3, 5);
+    addEdge(graph, 3, 4, 3);
+    addEdge(graph, 4, 5, 6);
+    addEdge(graph, 1, 6, 8);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 7, 8, 7);
+    addEdge(graph, 8, 9, 2);
+    addEdge(graph, 0, 9, 15);
+
+    int startVertex = 0;
+    vector<int> distances = dijkstra(graph, startVertex);
+
+    printResult(distances, startVertex);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    testDijkstra();
+
+    return 0;
+}
