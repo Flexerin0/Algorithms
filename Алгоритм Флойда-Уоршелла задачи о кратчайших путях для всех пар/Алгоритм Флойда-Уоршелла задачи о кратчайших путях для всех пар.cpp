1841fcd84456a0f3e104b530222de8d93ec84376
Flexerin0 <vfieliks@mail.ru>
2024-12-19 23:13:16 +0300

    Added another algorithms2.0

---
+++ Алгоритм Флойда-Уоршелла задачи о кратчайших путях для всех пар/Алгоритм Флойда-Уоршелла задачи о кратчайших путях для всех пар.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+#include <iomanip>
+
+using namespace std;
+
+const double INF = numeric_limits<double>::infinity();
+
+vector<vector<vector<double>>> algorithmFloydThreeArray(const vector<vector<double>>& graph) {
+    int n = graph.size();
+    vector<vector<vector<double>>> A(n + 1, vector<vector<double>>(n, vector<double>(n, INF)));
+
+    for (int v = 0; v < n; ++v) {
+        for (int w = 0; w < n; ++w) {
+            if (v == w) {
+                A[0][v][w] = 0;
+            }
+            else if (graph[v][w] != INF) {
+                A[0][v][w] = graph[v][w];
+            }
+            else {
+                A[0][v][w] = INF;
+            }
+        }
+    }
+
+    for (int k = 1; k <= n; ++k) {
+        for (int v = 0; v < n; ++v) {
+            for (int w = 0; w < n; ++w) {
+                A[k][v][w] = min(A[k - 1][v][w], A[k - 1][v][k - 1] + A[k - 1][k - 1][w]);
+            }
+        }
+    }
+
+    for (int v = 0; v < n; ++v) {
+        if (A[n][v][v] < 0) {
+            throw runtime_error("Граф содержит отрицательный цикл");
+        }
+    }
+
+    return A;
+}
+
+void testAlgorithmFloydThreeArray() {
+    vector<vector<double>> graph = {
+        {0, 3, INF, INF},
+        {INF, 0, 1, INF},
+        {INF, INF, 0, 7},
+        {2, INF, INF, 0}
+    };
+
+    auto result = algorithmFloydThreeArray(graph);
+
+    cout << "Кратчайшие пути между всеми парами вершин:\n";
+    for (int v = 0; v < graph.size(); ++v) {
+        for (int w = 0; w < graph.size(); ++w) {
+            if (result[graph.size()][v][w] == INF) {
+                cout << "INF ";
+            }
+            else {
+                cout << setw(4) << result[graph.size()][v][w] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    testAlgorithmFloydThreeArray();
+
+    return 0;
+}
---
+++ Алгоритм задачи поиска оптимального дерева поиска/Алгоритм задачи поиска оптимального дерева поиска.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+const double INF = numeric_limits<double>::infinity();
+
+double optimalBST(const vector<double>& freq) {
+    int n = freq.size();
+    vector<vector<double>> A(n + 2, vector<double>(n + 1, 0.0));
+
+    for (int i = 1; i <= n + 1; ++i) {
+        A[i][i - 1] = 0;
+    }
+
+    for (int s = 0; s < n; ++s) {
+        for (int i = 1; i <= n - s; ++i) {
+            int j = i + s;
+            A[i][j] = INF;
+
+            double frequencySum = 0.0;
+            for (int k = i; k <= j; ++k) {
+                frequencySum += freq[k - 1];
+            }
+
+            for (int r = i; r <= j; ++r) {
+                double cost = A[i][r - 1] + A[r + 1][j] + frequencySum;
+                if (cost < A[i][j]) {
+                    A[i][j] = cost;
+                }
+            }
+        }
+    }
+
+    return A[1][n];
+}
+
+void testOptimalBST() {
+    vector<double> frequency = { 0.15, 0.10, 0.05, 0.10, 0.20 };
+
+    double result = optimalBST(frequency);
+
+    cout << "Минимальное взвешенное время поиска: " << result << endl;
+
+    cout << "\nТест с большим объемом данных...\n";
+    vector<double> largeFreq(1000, 0.001);
+    result = optimalBST(largeFreq);
+    cout << "Минимальное взвешенное время поиска для 1000 ключей: " << result << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    testOptimalBST();
+
+    return 0;
+}
